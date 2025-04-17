5f8cc2806f08b936f3060fbf173fa8ebb38a28e4
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-04-17 18:50:05 +0400

    commit it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Home = () => {
   const [data, setData] = useState([]);
+
+  const getData =()=> {
+    axios.get('')
+    
+  }
 
   
   return <div></div>;

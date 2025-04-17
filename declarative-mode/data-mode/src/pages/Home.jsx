4cfbdf84e86ac86dee92ee1ea01238ec451af800
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-04-18 00:50:52 +0400

    Now ready to start the two projects

---
+++ declarative-mode/data-mode/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  const getData =()=> {
+    axios.get('')
+    
+  }
+
+  
+  return <div></div>;
+};
+
+export default Home;
